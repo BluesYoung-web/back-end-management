dde24382f9c7dab042c8e4b283916a62c99ca4a7
张扬 <15171255945@163.com>
2020-07-17 09:52:05 +0800

    Create index.vue

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 控制面板 -->
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from '@/store/sessionStorage/index';
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      name: ''
+    };
+  },
+  mounted() {
+    this.name = getUserInfo().real_name;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
